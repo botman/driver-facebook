4b4a7cc48411a7db9654615b54fb355078b2663e
Marcel Pociot <m.pociot@gmail.com>
2017-06-08 17:23:52 +0200

    Add discovery.json and config stubs

---
+++ stubs/facebook.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Facebook Token
+    |--------------------------------------------------------------------------
+    |
+    | Your Facebook application you received after creating
+    | the messenger page / application on Facebook.
+    |
+    */
+    'facebook_token' => env('FACEBOOK_TOKEN'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Facebook App Secret
+    |--------------------------------------------------------------------------
+    |
+    | Your Facebook application secret, which i sued to verify
+    | incoming requests from Facebook.
+    |
+    */
+    'facebook_app_secret' => env('FACEBOOK_APP_SECRET'),
+];
