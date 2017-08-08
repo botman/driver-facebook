e6993edffc5d1a81cc2570c7f4607025a88bcb69
Christoph Rumpel <c.rumpel@kabsi.at>
2017-08-08 21:24:48 +0200

    Fix styles

--- tests/FacebookDriverTest.php
+++ tests/FacebookDriverTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Drivers;
 
-use BotMan\Drivers\Facebook\Exceptions\FacebookException;
 use Mockery as m;
 use BotMan\BotMan\Http\Curl;
 use PHPUnit_Framework_TestCase;
@@ -22,6 +21,7 @@ use BotMan\Drivers\Facebook\Events\MessagingOptins;
 use BotMan\BotMan\Messages\Incoming\IncomingMessage;
 use BotMan\Drivers\Facebook\Events\MessagingReferrals;
 use BotMan\Drivers\Facebook\Events\MessagingDeliveries;
+use BotMan\Drivers\Facebook\Exceptions\FacebookException;
 use BotMan\Drivers\Facebook\Events\MessagingCheckoutUpdates;
 
 class FacebookDriverTest extends PHPUnit_Framework_TestCase
