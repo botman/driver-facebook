8071f777388de7cde8122a340acdab01694d517b
Romain Monteil <monteil.romain@gmail.com>
2018-10-04 08:45:12 +0200

    Fix OpenGraph template and element class, fix tests (#82)
    * Fix OpenGraph template and element class, fix tests
    
    * Fix variables name for OpenGraphElementTest
    
    * Revert setUrl() to url()

--- tests/Extensions/OpenGraphElementTest.php
+++ tests/Extensions/OpenGraphElementTest.php
@@ -12,29 +12,19 @@ class OpenGraphElementTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function it_can_be_created()
     {
-        $button = new OpenGraphElement('Rick Roll');
-        $this->assertInstanceOf(Element::class, $button);
+        $element = new OpenGraphElement();
+        $this->assertInstanceOf(OpenGraphElement::class, $element);
     }
 
     /**
      * @test
      **/
-    public function it_can_set_title()
+    public function it_can_set_an_url()
     {
-        $element = new Element('Rick Roll');
+        $element = new OpenGraphElement();
+        $element->url('https://example.com');
 
-        $this->assertSame('Rick Roll', Arr::get($element->toArray(), 'title'));
-    }
-
-    /**
-     * @test
-     **/
-    public function it_can_set_url()
-    {
-        $element = new Element('Rick Roll');
-        $element->url('https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb');
-
-        $this->assertSame('https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb', Arr::get($element->toArray(), 'url'));
+        $this->assertSame('https://example.com', Arr::get($element->toArray(), 'url'));
     }
 
     /**
@@ -42,12 +32,10 @@ class OpenGraphElementTest extends PHPUnit_Framework_TestCase
      **/
     public function it_can_add_a_button()
     {
-        $template = new Element('Here are some buttons');
-        $template->addButton(ElementButton::create('button1')
-                                ->url('https://en.wikipedia.org/wiki/Rickrolling')
-                                ->removeHeightRatio());
+        $element = new OpenGraphElement();
+        $element->addButton(ElementButton::create('button1')->url('https://en.wikipedia.org/wiki/Rickrolling')->removeHeightRatio());
 
-        $this->assertSame('button1', Arr::get($template->toArray(), 'buttons.0.title'));
+        $this->assertSame('button1', Arr::get($element->toArray(), 'buttons.0.title'));
     }
 
     /**
@@ -55,15 +43,13 @@ class OpenGraphElementTest extends PHPUnit_Framework_TestCase
      **/
     public function it_can_add_multiple_buttons()
     {
-        $template = new Element('Here are some buttons');
-        $template->addButtons([ElementButton::create('button1')
-                                ->url('https://en.wikipedia.org/wiki/Rickrolling')
-                                ->removeHeightRatio(),
-                               ElementButton::create('button2')
-                                ->url('https://en.wikipedia.org/')
-                                ->removeHeightRatio(), ]);
+        $element = new OpenGraphElement();
+        $element->addButtons([
+            ElementButton::create('button1')->url('https://en.wikipedia.org/wiki/Rickrolling')->removeHeightRatio(),
+            ElementButton::create('button2')->url('https://en.wikipedia.org/')->removeHeightRatio(),
+        ]);
 
-        $this->assertSame('button1', Arr::get($template->toArray(), 'buttons.0.title'));
-        $this->assertSame('button2', Arr::get($template->toArray(), 'buttons.1.title'));
+        $this->assertSame('button1', Arr::get($element->toArray(), 'buttons.0.title'));
+        $this->assertSame('button2', Arr::get($element->toArray(), 'buttons.1.title'));
     }
 }
