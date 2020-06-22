3f08a5c5c03f9334b91a8e338e836f60c3a722b8
apreiml <apreiml@strohwolke.at>
2020-06-22 11:08:24 +0200

    fix trying to access offset 0 on type null (#99)

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -89,7 +89,7 @@ class FacebookDriver extends HttpDriver implements VerifiesService
     public function buildPayload(Request $request)
     {
         $this->payload = new ParameterBag((array) json_decode($request->getContent(), true));
-        $this->event = Collection::make((array) $this->payload->get('entry')[0]);
+        $this->event = Collection::make((array) $this->payload->get('entry', [null])[0]);
         $this->signature = $request->headers->get('X_HUB_SIGNATURE', '');
         $this->content = $request->getContent();
         $this->config = Collection::make($this->config->get('facebook', []));
